06c6b7f252c244a733de1640c5da6f23492b37f0
andixeneize <ae.sanchez89@gmail.com>
2021-02-05 13:41:12 -0300

    Se agregan keys a Breadcrumbs y se separan sus estilos en el componente

--- components/breadcrumbs.tsx
+++ components/breadcrumbs.tsx
@@ -6,16 +6,25 @@ const Breadcrumbs = ({ categories }) => {
     if (categories && categories.length) {
         breadcrumbs = categories.map((category, index) => {
             if (index == 0) {
-                return <span>{category.name}</span>
+                return <span key={index} >{category.name}</span>
             } else {
-                return <span> &gt; {category.name}</span>
+                return <span key={index}> &gt; {category.name}</span>
             }
         });
     } 
 
     return (
-        <div className={styles.bread}>   
+        <div className="bread">   
             {breadcrumbs}
+            <style jsx>{`
+                .bread {
+                    width: 80vw;
+                    justify-content: left;
+                    margin: 16px 0 16px;
+                    color: #333333;
+                    font-size: 14px;
+                }
+            `}</style>
         </div>
     )
 };
